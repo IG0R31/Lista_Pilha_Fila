ee83dd8eb82adf0823b8ecb5f94c63d7916e3b18
Igor Oliveira Duarte <igor.oliveiraduarte05@gmail.com>
2025-01-05 12:22:27 -0300

    parte2

--- lista_duplamente_ligada.c
+++ lista_duplamente_ligada.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef int TIPOCHAVE;
-typedef struct 
-{
+typedef struct {
 TIPOCHAVE chave;
 // outros campos...
 } REGISTRO;
 
-typedef struct aux 
-{
+typedef struct aux {
     REGISTRO reg;
     struct aux *ant, *prox;
 } ELEMENTO;
 
 typedef ELEMENTO* PONT;
-typedef struct 
-{
+typedef struct {
     PONT inicio;
 } LISTA;
 
-void inicializar(LISTA *l)
-{
+void inicializarLista(LISTA *l){
     l->inicio = NULL; 
 }
-int inserirElemListaOrd(LISTA* l, REGISTRO reg);
+
+
+
+bool inserirElemListaOrd(LISTA* l, REGISTRO reg)
 {
     PONT novo = (PONT)malloc(sizeof(ELEMENTO));
     if(novo==NULL) return false; 
@@ -38,5 +38,50 @@ int inserirElemListaOrd(LISTA* l, REGISTRO reg);
     }
     PONT atual = l->inicio;
     PONT anterior = NULL; 
+
+    while(atual != NULL && atual->reg.chave < reg.chave){
+        anterior = atual;
+        atual = atual->prox;
+    }
+    if(anterior == NULL){
+        novo->prox = l->inicio;
+        l->inicio = novo;
+    }
+    else{
+        novo->prox=  atual;
+        novo->ant= anterior; 
+        anterior ->prox = novo;
+
+        if (atual != NULL){
+            atual->ant = novo;
+        }
+    }
+    return true;
     
+}
+
+void exibirLista(LISTA *l)
+{
+    PONT atual = l->inicio;
+    while(atual != NULL){
+        printf("(%d)", atual->reg.chave);
+        atual = atual->prox;
+    }
+    printf("\n");
+
+}
+
+int main(){
+    LISTA l;
+    inicializarLista(&l);
+    REGISTRO r1 = {62};
+    REGISTRO r2 = {19};
+    REGISTRO r3 = {88};
+    inserirElemListaOrd(&l, r1);
+    inserirElemListaOrd(&l, r2);
+    inserirElemListaOrd(&l, r3);
+
+    printf("Lista após inserções ordenadas: ");
+    exibirLista(&l);
+    return 0; 
 }
